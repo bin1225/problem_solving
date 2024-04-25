500a55ae2bfbf3875c443f6ed56c48724ce8eff5
bin1225 <gimgyubin1225@gmail.com>
2024-04-26 03:20:14 +0900

    BOJ 8980

--- .vscode/BOJ/8980_a.cpp
+++ .vscode/BOJ/8980_a.cpp
@@ -9,48 +9,54 @@
 using namespace std;
 
 int N, C, M;
-int capacity[2020];
-struct Box{
-    int start, end, count;
-};
 
-bool cmp(const Box& a, const Box& b){
-    //if(a.end == b.end) return a.start < b.start;
-    return a.end < b.end;
-}
 
 void Solve() {
     cin>>N>>C;
     cin>>M;
-    vector<Box> V;
+    vector<pair<int,int>> V[2020];
     int a,b,c;
     int nowLoc = 0, nowCount = 0, answer = 0, idx = 0;
 
     for(int i=0; i<M; i++){
         cin>>a>>b>>c;
-        V.push_back({a,b,c});
+        V[a].push_back({b,c});
     }
 
-    sort(V.begin(), V.end(), cmp);
-
-    for(int i=0; i<M; i++){
-        
-        int maxCount = 0;
-        for(int j = V[i].start; j<V[i].end; j++){
-            maxCount = max(maxCount, capacity[j]);
-        }
-
-        int availCount = min(V[i].count, C - maxCount);
-
-        for(int j = V[i].start; j<V[i].end; j++){
-            capacity[j]+=availCount;
-        }
-        answer+=availCount;
+    for(int i=0; i<=N; i++){
+        sort(V[i].begin(), V[i].end());
     }
 
+    priority_queue<pair<int,int>> PQ;
+
+    for(int i=0; i<=N; i++){
+
+        //i번째에서 내리는 택배를 다 내린다.
+        while(!PQ.empty() && PQ.top().first == -i){
+            //cout<<i<<" "<<PQ.top().second<<endl;
+            answer+=PQ.top().second; PQ.pop();
+        }
+
+        //i번째에서 보내는 택배를 일단 다 실는다.
+        for(int j=0; j<V[i].size(); j++){
+            PQ.push({-V[i][j].first, V[i][j].second});
+        }
+
+        //트럭 용량을 맞춘다.
+        vector<pair<int,int>> V;
+        nowCount = 0;
+        while(!PQ.empty() && nowCount<C){
+            pair<int,int> p = PQ.top(); PQ.pop();
+            int count = min(C-nowCount, p.second);
+            nowCount+=count;
+            V.push_back({p.first, count});
+        }
+
+        while(!PQ.empty()) PQ.pop();
+        for(int i=0; i<V.size(); i++) PQ.push(V[i]);
+
+    }
     cout<<answer;
-
-
 }
 
 
