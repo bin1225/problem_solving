e013e8241a6b83d17d2233a7265f059062da7390
bin1225 <gimgyubin1225@gmail.com>
2024-04-26 02:58:31 +0900

    BOJ 8980 더 좋은 풀이

---
+++ .vscode/BOJ/8980_a.cpp
@@ -0,0 +1,64 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+
+#define endl "\n"
+
+using namespace std;
+
+int N, C, M;
+int capacity[2020];
+struct Box{
+    int start, end, count;
+};
+
+bool cmp(const Box& a, const Box& b){
+    //if(a.end == b.end) return a.start < b.start;
+    return a.end < b.end;
+}
+
+void Solve() {
+    cin>>N>>C;
+    cin>>M;
+    vector<Box> V;
+    int a,b,c;
+    int nowLoc = 0, nowCount = 0, answer = 0, idx = 0;
+
+    for(int i=0; i<M; i++){
+        cin>>a>>b>>c;
+        V.push_back({a,b,c});
+    }
+
+    sort(V.begin(), V.end(), cmp);
+
+    for(int i=0; i<M; i++){
+        
+        int maxCount = 0;
+        for(int j = V[i].start; j<V[i].end; j++){
+            maxCount = max(maxCount, capacity[j]);
+        }
+
+        int availCount = min(V[i].count, C - maxCount);
+
+        for(int j = V[i].start; j<V[i].end; j++){
+            capacity[j]+=availCount;
+        }
+        answer+=availCount;
+    }
+
+    cout<<answer;
+
+
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solve();
+
+    return 0;
+}
