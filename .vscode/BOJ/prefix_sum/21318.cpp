ef64b7cd3098524a5a3525d4575d55dd1752b47c
bin1225 <gimgyubin1225@gmail.com>
2024-10-03 23:59:47 +0900

    BOJ 21318 silver1 prefix_sum

---
+++ .vscode/BOJ/prefix_sum/21318.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+
+#define endl "\n"
+#define ll long long
+
+using namespace std;
+
+int A[101010];
+int PS[101010];
+
+void Solve() {
+    int N; cin>>N;
+    for(int i=1; i<=N; i++){
+        cin>>A[i];
+    }
+    A[N+1] = INT_MAX;
+
+    for(int i=1; i<=N; i++){
+        PS[i] = PS[i-1];
+        if(A[i]>A[i+1]) PS[i]++;
+    }
+    int Q,x,y,cnt; cin>>Q;
+    vector<int> ans;
+    for(int i=1; i<=Q; i++){
+        cin>>x>>y;
+        ans.push_back(PS[y-1]-PS[x-1]);
+    }
+
+    for(int a: ans) cout<<a<<endl;
+}
+
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    Solve();
+
+    return 0;
+}
