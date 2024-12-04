905475a98873fa2325cb8c54c543324d56ac986c
kkb <gimgyubin1225@gmail.com>
2024-12-04 13:03:38 +0900

    BOJ 14503 로봇청소기 gold4 implement

--- cpp/.vscode/BOJ/implement/14503.cpp
+++ cpp/.vscode/BOJ/implement/14503.cpp
@@ -5,91 +5,46 @@
 
 using namespace std;
 
+void claen(int r, int c, int d);
 int Room[50][50];
-int dx[] = {-1,0,1,0};
-int dy[] = {0,1,0,-1};
-int N,M,r,c,d,ans; 
+int dr[] = {-1,0,1,0};
+int dc[] = {0,1,0,-1};
+int ans = 0; 
 
-void clean_room(){
+void clean(int r, int c, int d){
     if(Room[r][c]==0) {
         Room[r][c] = 2;
         ans++;
     }
-}
 
-bool check(){
     for(int i=0; i<4; i++){
-        if(Room[r+dy[i]][c+dx[i]] == 0) return true;
+        d = (d+3)%4;
+        int nr = r+dr[d];
+        int nc = c+dc[d];
+
+        if(Room[nr][nc] == 0){
+            clean(nr,nc,d);
+            return;
+        } 
     }
-    return false;
+
+    int back = (d+2)%4;
+    int br = r+dr[back];
+    int bc = c+dc[back];
+
+    if(Room[br][bc]!=1){
+        clean(br,bc,d);
+    }
 }
 
-//빈 칸이 없는 경우
-bool move1(){
-    switch(d){
-        case 0:
-            if(Room[r+1][c]!=1){
-                r++;
-                return true;
-            }
-        break;
-        case 1:
-            if(Room[r][c-1]!=1) {
-                c--;
-                return true;
-            }
-        break;
-        case 2:
-            if(Room[r-1][c]!=1) {
-                r--;
-                return true;
-            }
-        break;
-        case 3:
-            if(Room[r][c+1]!=1) {
-                c++;
-                return true;
-            }
-        break;
-    }
-    return false;
-}
-
-//빈 칸이 있는 경우
-void move2(){
-    switch(d){
-        case 0:
-            d=3;
-            if(Room[r][c-1] == 0) c--;
-        break;
-        case 1:
-            d=0;
-            if(Room[r-1][c] == 0) r--;
-        break;
-        case 2:
-            d=1;
-            if(Room[r][c+1] == 0) c++;
-        break;
-        case 3:
-            d=2;
-            if(Room[r+1][c] == 0) r++;
-        break;
-    }
-}
 void Solve() {
-    cin>>N>>M>>r>>c>>d;
-
+    int N,M,r,c,d; cin>>N>>M>>r>>c>>d;
     for(int i=0; i<N; i++){
         for(int j=0; j<M; j++){
             cin>>Room[i][j];
         }
     }   
-
-    while(true){
-        clean_room();
-        if(check()) move2();
-        else if(move1() == false) break;
-    }
+    clean(r,c,d);
     cout<<ans;
 }
 
