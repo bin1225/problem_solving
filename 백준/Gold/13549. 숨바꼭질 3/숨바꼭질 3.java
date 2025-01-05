24df6ecf431bba21affdaf1450137febb7487cc5
bin1225 <gimgyubin1225@gmail.com>
2025-01-05 11:55:09 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 160 ms, Memory: 17264 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -7,6 +7,21 @@ import java.util.StringTokenizer;
 
 public class Main {
 
+    static class Loc implements Comparable<Loc> {
+        int now;
+        int time;
+
+        Loc(int now, int time) {
+            this.now = now;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Loc o) {
+            return this.time - o.time;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -16,32 +31,35 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
         K = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> PQ = new PriorityQueue<>();
+        PriorityQueue<Loc> PQ = new PriorityQueue<>();
         int[] visited = new int[100001];
         Arrays.fill(visited, 987654321);
-
-        PQ.add(N);
+        PQ.add(new Loc(N, 0));
         visited[N] = 0;
 
         while (!PQ.isEmpty()) {
-            int x = PQ.poll();
-            int time = visited[x];
+            Loc loc = PQ.poll();
+            int x = loc.now;
+            int time = loc.time;
 
+            if (x == K) {
+                System.out.println(time);
+                return;
+            }
             if (x * 2 <= 100000 && time < visited[x * 2]) {
                 visited[x * 2] = time;
-                PQ.add(x*2);
+                PQ.add(new Loc(x * 2, time));
             }
             if (x + 1 <= 100000 && time + 1 < visited[x + 1]) {
                 visited[x + 1] = time + 1;
-                PQ.add(x+1);
+                PQ.add(new Loc(x + 1, time + 1));
             }
             if (x - 1 >= 0 && time + 1 < visited[x - 1]) {
                 visited[x - 1] = time + 1;
-                PQ.add(x-1);
+                PQ.add(new Loc(x - 1, time + 1));
             }
-        }
 
-        System.out.println(visited[K]);
+        }
     }
 
 }
