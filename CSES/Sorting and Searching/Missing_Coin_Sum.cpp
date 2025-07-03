7d151fd8a731e97ab7b86e8843444693bbd62e15
kkb <gimgyubin1225@gmail.com>
2025-07-03 15:14:35 +0900

    CSES Sorting and Searching 3try

---
+++ CSES/Sorting and Searching/Missing_Coin_Sum.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+
+#define ll long long
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+    vector<ll> v;
+    for (int i = 0; i < n; i++) {
+        ll a;
+        cin >> a;
+        v.push_back(a);
+    }
+
+    sort(v.begin(), v.end());
+    ll now = 1;
+    for (int i = 0; i < n; i++) {
+        if(v[i]>now)
+            break;
+        else {
+            now += v[i];
+        }
+    }
+    cout << now;
+    return 0;
+}
